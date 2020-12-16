fe46407e004b0f5565d291da1bfbf9ca38107825
karthikp63 <karthik.tej.pasupuleti@gmail.com>
2020-12-15 16:59:49 -0800

    angry birds

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg,platform, constrainedLog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,7 +15,8 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+    constrainedLog = new Log(230,180, 80, PI/2);
+    
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -35,7 +36,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    chain=new Chain(bird.body,constrainedLog);
 }
 
 function draw(){
@@ -61,4 +62,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    constrainedLog.display();
+    chain.display();
 }
